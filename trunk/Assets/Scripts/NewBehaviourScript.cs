e3294f0439c2163b9f8d8429f6154fe67ef506a1
feddas00@gmail.com <feddas00@gmail.com@cb771117-6fd7-5cea-ac1b-478de51e5eac>
2013-12-07 22:20:54 +0000

    blue cube making script

---
+++ trunk/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public Transform cube;
+
+	// Use this for initialization
+	void Start ()
+	{
+		var test = Instantiate(cube) as Transform;
+		Vector3 pos = new Vector3(0,0,0);
+		test.transform.position = pos;
+		test.renderer.material.color = Color.blue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//Time.deltaTime
+		if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+			;
+	}
+}
