e26fd16202d76faceade0c8465af800149bbbe72
feddas00@gmail.com <feddas00@gmail.com@cb771117-6fd7-5cea-ac1b-478de51e5eac>
2013-12-08 08:00:57 +0000

    Convert to sprites instead of cubes

---
+++ trunk/Assets/Scripts/Surgery/VitalOrgan.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VitalOrgan : MonoBehaviour
+{
+	public ParticleSystem bloodsplat;
+
+	void Start()
+	{
+	
+	}
+
+	void Update()
+	{
+	
+	}
+
+	private void OnCollisionEnter2D(Collision2D hitInfo)
+	{
+		if (bloodsplat == null)
+			return;
+
+		foreach (var contact in hitInfo.contacts)
+		{
+			Vector3 contact3d = new Vector3(contact.point.x, contact.point.y, 0);
+
+			// Set the sorting layer of the particle system. http://answers.unity3d.com/questions/579490/unity-43-particle-system-not-visible-in-2d-mode.html
+			bloodsplat.renderer.sortingLayerName = "Foreground";
+			bloodsplat.renderer.sortingOrder = 2;
+			Instantiate(bloodsplat, contact3d, this.transform.rotation);
+		}
+	}
+}
