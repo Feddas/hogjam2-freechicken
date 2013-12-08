76d3b17399f9ccb6813f055e99105630469c754b
feddas00 <feddas00@gmail.com>
2013-12-08 08:00:57 +0000

    Convert to sprites instead of cubes

---
+++ Assets/Scripts/Surgery/PartTracker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PartTracker : MonoBehaviour
+{
+	public static Transform CurrentPart;
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (CurrentPart != null)
+		{
+			movePart(CurrentPart);
+		}
+	}
+
+	private void movePart(Transform CurrentPart)
+	{
+		// Construct a ray from the current mouse coordinates
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+		Vector3 pos2D = ray.origin;
+		pos2D.z = 0;
+		CurrentPart.position = pos2D;
+	}
+
+}
