755311567ac5390f365a41fda4a11c46d2cf8873
joshayc <joshayc@gmail.com>
2013-12-08 13:23:24 +0000

    Added Frogger (Seems that including it in the existing project has caused the resolution to be weird. John said Sean knew a fix for this)

---
+++ Assets/Scripts/Frogger/PlayerControls.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour
+{
+	public static float moveForce = 365f;			// Amount of force added to move the player left and right.
+	public static float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
+	//private Animator anim;				// Reference to the player's animator component.
+	
+	void Awake()
+	{
+
+	}
+
+	void Update()
+	{
+		float h = rigidbody2D.velocity.x;
+		float v = rigidbody2D.velocity.y;
+
+		float tiltAroundZ = Mathf.Atan2(v, h) * (180f / Mathf.PI) + (90f * Mathf.PI / 180f);
+		var target = Quaternion.Euler(0, 0, tiltAroundZ);
+		
+		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 6.0F);
+
+	}
+
+	void FixedUpdate ()
+	{
+		// Cache the horizontal input.
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		
+		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+		if(h * rigidbody2D.velocity.x < maxSpeed)
+			// ... add a force to the player.
+			rigidbody2D.AddForce(Vector2.right * h * moveForce);
+		if(v * rigidbody2D.velocity.y < maxSpeed)
+			// ... add a force to the player.
+			rigidbody2D.AddForce(Vector2.up * v * moveForce);
+		
+		// If the player's horizontal velocity is greater than the maxSpeed...
+		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+			// ... set the player's velocity to the maxSpeed in the x axis.
+			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+		if(Mathf.Abs(rigidbody2D.velocity.y) > maxSpeed)
+			// ... set the player's velocity to the maxSpeed in the x axis.
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, Mathf.Sign(rigidbody2D.velocity.y) * maxSpeed);
+	
+	}
+}
---
+++ Assets/Scripts/Frogger/Spawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+
+	public float spawnTime = 3f;		// The amount of time between each spawn.
+	public float spawnDelay = 0f;		// The amount of time before spawning starts.
+	public float spawnX;
+	public float spawnSpeed = 10;
+	public GameObject[] enemies;		// Array of enemy prefabs.
+
+	void Start ()
+	{
+		// Start calling the Spawn function repeatedly after a delay .
+		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+	}
+
+	void Spawn ()
+	{
+		// Instantiate a random enemy.
+
+		int enemyIndex = Random.Range(0, enemies.Length);
+
+		var clone = Instantiate(enemies[enemyIndex], transform.position, Quaternion.identity);
+
+		Fire((GameObject) clone);
+
+		// Play the spawning effect from all of the particle systems.
+		foreach(ParticleSystem p in GetComponentsInChildren<ParticleSystem>())
+		{
+			p.Play();
+		}
+	}
+	
+
+	void Fire (GameObject go)
+	{
+		go.rigidbody2D.velocity = new Vector2 (spawnX * spawnSpeed, 0);
+
+		Vector3 scale = go.transform.localScale;
+		scale.x *= spawnX;
+		go.transform.localScale = scale;
+	}
+}
