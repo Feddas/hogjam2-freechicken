fd31bccb108a11f2dfbcaaa16f76347beb8b85a7
feddas00 <feddas00@gmail.com>
2013-12-08 03:08:49 +0000

    2xCube surgery tools

--- Assets/Scripts/Surgery/RayCast.cs
+++ Assets/Scripts/Surgery/RayCast.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RayCast : MonoBehaviour {
+public class RayCast : MonoBehaviour
+{
 	public Transform Knife;
+	public ParticleSystem bloodsplat;
+	public Transform currentTool;
+
+	public const string TagTool = "Tool";
+	public const string TagOrgan = "VitalOrgan";
 
 	void Start ()
 	{
@@ -11,31 +17,17 @@ public class RayCast : MonoBehaviour {
 
 	void Update()
 	{
-		if (Knife == null)
-			return;
-
-		// Construct a ray from the current mouse coordinates
-		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		
-		//Debug.DrawLine(ray.origin, ray.direction*1, Color.cyan);
-		
-		Knife.position = ray.origin;
-		RaycastHit hit = new RaycastHit();
-		if (Physics.Raycast(ray, out hit))
+		if (currentTool != null)
 		{
-			hit.transform.renderer.material.color = Color.red;
-
-			// Create a particle if hit
-			//Instantiate (particle, transform.position, transform.rotation);
-			Debug.Log("hit");
-			Destroy(Knife.gameObject);
+			holdingTool();
 		}
 		else
 		{
+			if (Input.GetButtonDown("Fire1"))
+			{
+				selectATool();
+			}
 		}
-
-		//camera.ViewportPointToRay(Input.mousePosition);
-//		//knife.position = Input.mousePosition;
 //		if (Input.GetButtonDown ("Fire1"))
 //		{
 //			RaycastHit hit = new RaycastHit();
@@ -48,5 +40,42 @@ public class RayCast : MonoBehaviour {
 //			}
 //			Debug.DrawLine (transform.position, hit.point, Color.cyan);
 //		}
+	}
+
+	private void holdingTool()
+	{
+//		if (Knife == null)
+//			return;
+		
+		// Construct a ray from the current mouse coordinates
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		Vector3 pos2D = ray.origin;
+		pos2D.z = 0;
+		currentTool.position = pos2D;
+		RaycastHit hit = new RaycastHit();
+		if (Physics.Raycast(ray, out hit) && hit.transform.tag == TagOrgan)
+		{
+			//Debug.Log("hit" + Vector3.up + "; " + hit.normal.ToString());
+			Destroy(currentTool.gameObject);
+			currentTool = null;
+			
+			// Create blood
+			hit.transform.renderer.material.color = Color.red;
+			var hitRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+			Instantiate(bloodsplat, ray.origin, hitRotation);
+		}
+	}
+
+	private void selectATool()
+	{
+		// Construct a ray from the current mouse coordinates
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		RaycastHit hit = new RaycastHit();
+		if (Physics.Raycast(ray, out hit) && hit.transform.tag == TagTool)
+		{
+			currentTool = hit.transform;
+		}
 	}
 }
